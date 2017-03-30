55f69c392c5b8894ddf6355b5dd1e37d40732dda
magomedgadjiev <magomedalievic@gmail.com>
2017-03-30 20:32:02 +0300

    update

--- src/main/java/application/controller/UserControllerWithDB.java
+++ src/main/java/application/controller/UserControllerWithDB.java
@@ -1,35 +1,41 @@
 package application.controller;
 
-import java.io.IOException;
-import java.util.List;
-
-import application.models.RespWithUsers;
-import application.accountService.AccountService;
-import application.models.RespWithUser;
 import application.models.Resp;
+import application.models.RespWithUser;
+import application.models.RespWithUsers;
+import application.user.UserProfile;
+import application.user.UserProfileJDBCTemplate;
 import com.fasterxml.jackson.core.JsonParseException;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
-import application.user.UserProfile;
 
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
+/**
+ * Created by magomed on 29.03.17.
+ */
 @RestController
 @Component
-@RequestMapping(value = "/api")
-public class UserController {
+@RequestMapping(value = "/api/DB")
+public class UserControllerWithDB {
     public static final String EMAIL = "userEmail";
     public static final String LOGIN = "logIn";
     public static final String SUCCESS = "success";
 
-    private final static Logger LOGGER = Logger.getLogger("maga");
+    private static final Logger LOGGER = Logger.getLogger("UserControllerWithDB");
 
     @Autowired
-    private final AccountService accountService;
+    private UserProfileJDBCTemplate userProfileJDBCTemplate;
 
     @Autowired
     private HttpSession session;
@@ -50,17 +56,16 @@ public class UserController {
                 LOGGER.debug("Not all required parameters provided");
                 return new ResponseEntity<>(new Resp(2, "Not all required parameters provided"), HttpStatus.BAD_REQUEST);
             }
-            if (accountService.isSignUp(userProfile.getEmail(), userProfile.getPassword())) {
-                if (session.getAttribute(LOGIN) == null) {
-                    session.setAttribute(LOGIN, true);
-                    session.setAttribute(EMAIL, userProfile.getEmail());
-                }
-                LOGGER.debug("Success login");
-                return ResponseEntity.ok(new RespWithUser(0, accountService.getUser(userProfile.getEmail())));
+            userProfileJDBCTemplate.getUserProfile(userProfile.getEmail());
+            if (session.getAttribute(LOGIN) == null) {
+                session.setAttribute(LOGIN, true);
+                session.setAttribute(EMAIL, userProfile.getEmail());
             }
+            LOGGER.debug("Success login");
+            return ResponseEntity.ok(new RespWithUser(0, userProfileJDBCTemplate.getUserProfile(userProfile.getEmail())));
+        } catch (EmptyResultDataAccessException e) {
             LOGGER.debug("did't registration");
             return new ResponseEntity<>(new Resp(1, "You did't registration"), HttpStatus.BAD_REQUEST);
-
         } catch (RuntimeException ignored) {
             LOGGER.debug("Iternal server error");
             return new ResponseEntity<Resp>(new Resp(4, "Iternal server error"), HttpStatus.INTERNAL_SERVER_ERROR);
@@ -72,7 +77,7 @@ public class UserController {
         try {
             if (session.getAttribute(LOGIN) != null) {
                 LOGGER.debug("Success get info user");
-                return ResponseEntity.ok(new RespWithUser(0, (accountService.getUser((String) (session.getAttribute(EMAIL))))));
+                return ResponseEntity.ok(new RespWithUser(0, (userProfileJDBCTemplate.getUserProfile((String) (session.getAttribute(EMAIL))))));
             }
             LOGGER.debug("don't login");
             return new ResponseEntity<>(new Resp(1, "You don't login"), HttpStatus.BAD_REQUEST);
@@ -87,9 +92,14 @@ public class UserController {
     public ResponseEntity<?> setInfoUser(@RequestBody UserProfile userProfile) throws IOException {
         try {
             if (session.getAttribute(LOGIN) != null) {
-                if (userProfile.isEmpty()) {
-                    accountService.getUser(session.getAttribute(EMAIL).toString()).setLogin(userProfile.getLogin());
-                    accountService.getUser(session.getAttribute(EMAIL).toString()).setPassword(userProfile.getPassword());
+                if (!userProfile.isEmpty()) {
+                    userProfileJDBCTemplate.update(userProfile.getLogin(), userProfile.getPassword(), userProfile.getEmail(), userProfile.getRating());
+                    if (userProfile.getLogin() != null){
+                        userProfileJDBCTemplate.updateNickname(userProfile.getLogin(), userProfile.getEmail());
+                    }
+                    if(userProfile.getPassword() != null){
+                        userProfileJDBCTemplate.updatePassword(userProfile.getPassword(), userProfile.getEmail());
+                    }
                     LOGGER.debug("User data succesfully updated");
                     return ResponseEntity.ok(new Resp(0, "User data succesfully updated"));
                 }
@@ -97,10 +107,13 @@ public class UserController {
                 return new ResponseEntity<>(new Resp(2, "Not all required parameters provided"), HttpStatus.BAD_REQUEST);
             }
             LOGGER.debug("don't login");
-            return new ResponseEntity<Resp>(new Resp(1, "you don't login"), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new Resp(1, "you don't login"), HttpStatus.BAD_REQUEST);
+        } catch (DuplicateKeyException e) {
+            LOGGER.debug("This user alredy exist");
+            return new ResponseEntity<>(new Resp(3, "This user alredy exist"), HttpStatus.CONFLICT);
         } catch (RuntimeException ignored) {
             LOGGER.debug("Iternal server error");
-            return new ResponseEntity<Resp>(new Resp(4, "Iternal server error"), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new Resp(4, "Iternal server error"), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -111,15 +124,14 @@ public class UserController {
                 LOGGER.debug("Not all required parameters provided");
                 return new ResponseEntity<>(new Resp(2, "Not all required parameters provided"), HttpStatus.BAD_REQUEST);
             }
-            if (accountService.isSignUp(userProfile.getEmail(), userProfile.getPassword())) {
-                LOGGER.debug("This application.user alredy exist");
-                return new ResponseEntity<>(new Resp(3, "This application.user alredy exist"), HttpStatus.CONFLICT);
-            }
+            userProfileJDBCTemplate.create(userProfile.getLogin(), userProfile.getPassword(), userProfile.getEmail());
             session.setAttribute(LOGIN, true);
             session.setAttribute(EMAIL, userProfile.getEmail());
-            accountService.addUser(userProfile);
             LOGGER.debug("Success registration" + session.getId());
             return new ResponseEntity<RespWithUser>(new RespWithUser(0, userProfile), HttpStatus.CREATED);
+        } catch (DuplicateKeyException e) {
+            LOGGER.debug("This user alredy exist");
+            return new ResponseEntity<>(new Resp(3, "This user alredy exist"), HttpStatus.CONFLICT);
         } catch (RuntimeException ignored) {
             LOGGER.debug("Iternal server error");
             return new ResponseEntity<Resp>(new Resp(4, "Iternal server error"), HttpStatus.INTERNAL_SERVER_ERROR);
@@ -129,18 +141,30 @@ public class UserController {
     @RequestMapping(value = "/stats/{count}", method = RequestMethod.POST)
     public ResponseEntity<?> getMMR(@PathVariable(value = "count") int count, @RequestBody UserProfile userProfile) throws IOException {
         try {
-            if (count > accountService.getSize()) {
+            if (count > userProfileJDBCTemplate.getCount()) {
                 return new ResponseEntity<Resp>(new Resp(1, "count > countUser"), HttpStatus.BAD_REQUEST);
             }
-            List<UserProfile> userProfiles = accountService.sort();
+            List<UserProfile> userProfiles = userProfileJDBCTemplate.getUsers();
             RespWithUsers respWithUsers = new RespWithUsers();
+            Collections.sort(userProfiles, new Comparator<UserProfile>() {
+                @Override
+                public int compare(UserProfile o1, UserProfile o2) {
+                    if (o2.getRating() == o1.getRating()) {
+                        return 0;
+                    } else if (o2.getRating() > o1.getRating()) {
+                        return 1;
+                    } else {
+                        return -1;
+                    }
+                }
+            });
+
             for (int i = 0; i < count; ++i) {
                 respWithUsers.addUser(userProfiles.get(i));
             }
             respWithUsers.setKey(0);
             LOGGER.debug("Success get users");
             return ResponseEntity.ok(respWithUsers);
-
         } catch (RuntimeException ignored) {
             LOGGER.debug("Iternal server error");
             return new ResponseEntity<Resp>(new Resp(4, "Iternal server error"), HttpStatus.INTERNAL_SERVER_ERROR);
@@ -148,7 +172,9 @@ public class UserController {
     }
 
     @Autowired
-    public UserController(AccountService accountService) {
-        this.accountService = accountService;
+    public UserControllerWithDB(UserProfileJDBCTemplate userProfileJDBCTemplate) {
+        this.userProfileJDBCTemplate = userProfileJDBCTemplate;
+        userProfileJDBCTemplate.dropTable();
+        userProfileJDBCTemplate.createTable();
     }
 }
---
+++ src/test/java/application/test/controllers/UserControllerTest.java
@@ -0,0 +1,63 @@
+package application.test.controllers;
+
+import application.accountService.AccountService;
+import application.user.UserProfile;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+import static org.junit.Assert.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+/**
+ * Created by isopov on 29.09.16.
+ */
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@RunWith(SpringRunner.class)
+public class UserControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    private UserProfile userProfile;
+
+
+    @Before
+    public void init(){
+        userProfile = new UserProfile("login", "password", "email");
+    }
+
+    @Test
+    public void testMeRequiresLogin() {
+        UserProfile userProfile = new UserProfile("login", "password", "email");
+        ResponseEntity<UserProfile> meResp = restTemplate.postForEntity("/api/DB/auth/regirstration", userProfile, UserProfile.class);
+        assertEquals(HttpStatus.CREATED, meResp.getStatusCode());
+
+        UserProfile userProfile1 = new UserProfile("aa", "dd", "email");
+
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity("/api/DB/auth/signOut", String.class);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        UserProfile userProfile2 = new UserProfile();
+        userProfile2.setLogin("a");
+        userProfile2.setPassword("a");
+        responseEntity = restTemplate.postForEntity("/api/DB/auth/regirstration", userProfile2, String.class);
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+
+        responseEntity = restTemplate.postForEntity("/api/DB/auth/login", userProfile2, String.class);
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+
+        responseEntity = restTemplate.postForEntity("/api/DB/user/setInfoUser", userProfile2, String.class);
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+
+        responseEntity = restTemplate.getForEntity("/api/DB/user/getInfoUser", String.class);
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+    }
+}
